5a870eca6ef0400b9f6692342f086a6c1db410de
Spitzelpit <relativ.einstein.777@gmail.com>
2025-09-25 20:46:52 +0200

    added to gh

---
+++ main.js
@@ -0,0 +1,28 @@
+const BidContent = "Bid now";
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+function changecontent(target) {
+  const newcontent = createRandomString(target.innerHTML.length);
+  target.innerHTML = newcontent;
+}
+
+function shuffle_text(event) {
+  const target = event.target;
+
+  const interval = setInterval(function() {
+    changecontent(target);
+  }, 30);
+
+  setTimeout(function() {
+    clearInterval(interval);
+    target.innerHTML = BidContent;
+  }, 300);
+}
+
