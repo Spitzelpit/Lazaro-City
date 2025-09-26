b55a5d064528e16e5c8bd0bc3b761b58a36f7146
Spitzelpit <relativ.einstein.777@gmail.com>
2025-09-27 00:09:07 +0200

    idk

--- main.js
+++ main.js
@@ -25,3 +25,14 @@ function shuffle_text(event, BidContent) {
   }, 300);
 }
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
